fde43f4b71ed81f897f70c3f8c4815fa22157448
FasTrss <greyhollmen@gmail.com>
2023-06-14 15:43:20 +0300

    sdfsdf

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,57 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
 /* eslint-disable */
-import path from 'path';
-import url from 'url';
+import path from "path";
+import url from "url";
 
-import HTMLWebpackPlugin from 'html-webpack-plugin';
-
+import HTMLWebpackPlugin from "html-webpack-plugin";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
+const mode =
+  process.env.NODE_ENV === "production" ? "production" : "development";
 
 export default {
   //context: path.resolve(__dirname, 'src'),
   mode: mode,
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
   devServer: {
     open: true,
-    host: 'localhost',
+    host: "localhost",
   },
   plugins: [
     new HTMLWebpackPlugin({
-      template: 'index.html',
+      template: "index.html",
     }),
   ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
+        loader: "babel-loader",
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
+        type: "asset",
       },
 
-      { test: /\.css$/, 
-      use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      { test: /\.css$/, use: ["style-loader", "css-loader", "postcss-loader"] },
       {
         test: /\.scss$/,
-        use: ['css-loader', 'sass-loader', 'postcss-loader'],
+        use: ["css-loader", "sass-loader", "postcss-loader"],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
+        use: "url-loader?limit=10000",
       },
       {
         test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
+        use: "file-loader",
       },
     ],
   },
