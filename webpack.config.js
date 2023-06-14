7dcce85c8723f545b8e2933dab30a0a1da914029
FasTrss <greyhollmen@gmail.com>
2023-06-14 13:08:03 +0300

    dd html

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,49 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+/* eslint-disable */
+import path from 'path';
+import url from 'url';
+
+import HTMLWebpackPlugin from 'html-webpack-plugin';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const mode = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
 export default {
-  mode: process.env.NODE_ENV || 'development',
+  //context: path.resolve(__dirname, 'src'),
+  mode: mode,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+  },
+    plugins: [
+    new HTMLWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+
+      { test: /\.css$/, 
+      use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        use: ['css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -29,13 +54,5 @@ export default {
         use: 'file-loader',
       },
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'template.html',
-    }),
-  ],
-  output: {
-    clean: true,
   },
 };
