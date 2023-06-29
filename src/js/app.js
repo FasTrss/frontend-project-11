b49fe3cfc0eb85e93ac335d207224feb4c790e79
FasTrss <greyhollmen@gmail.com>
2023-06-29 23:25:12 +0300

    refactoring promises with async/await

--- src/js/app.js
+++ src/js/app.js
@@ -25,108 +25,110 @@ const handleError = (error) => {
   return error.message ?? 'unknown error';
 };
 
-const updatePosts = (state) => {
-  const update = state.feeds.map((feed) => getAxiosResponse(feed.link))
-    .then((response) => {
-      const { feed, posts } = parse(response.data.contents);
-      const linksFromState = state.posts
-        .filter(({ feedId }) => feedId === feed.id)
-        .map((post) => post.link);
-      const newPostsWithIds = posts
-        .filter(({ link }) => !linksFromState.includes(link))
-        .map((post) => ({ ...post, id: uniqueId(), feedId: feed.id }));
-      state.posts.unshift(...newPostsWithIds);
-    })
-    .catch((err) => console.log(err.message));
-  Promise.all(update).finally(() => setTimeout(() => updatePosts(state), 5000));
+const updatePosts = async (state) => {
+  try {
+    await Promise.all(
+      state.feeds.map(async (feed) => {
+        const response = await getAxiosResponse(feed.link);
+        const { feed: parsedFeed, posts } = parse(response.data.contents);
+        const linksFromState = state.posts
+          .filter(({ feedId }) => feedId === parsedFeed.id)
+          .map((post) => post.link);
+        const newPostsWithIds = posts
+          .filter(({ link }) => !linksFromState.includes(link))
+          .map((post) => ({ ...post, id: uniqueId(), feedId: parsedFeed.id }));
+        state.posts.unshift(...newPostsWithIds);
+      }),
+    );
+  } catch (error) {
+    console.log(error.message);
+  } finally {
+    setTimeout(() => updatePosts(state), 5000);
+  }
 };
 
-export default () => {
+export default async () => {
   const i18nInstance = i18next.createInstance();
-  i18nInstance
-    .init({
-      lng: 'ru',
-      debug: false,
-      resources,
-    })
-    .then(() => {
-      const initialState = {
-        ui: {
-          submitDisabled: false,
-          readedPosts: new Set(),
-          modalWindow: null,
-        },
-        formStatus: 'filling',
-        error: '',
-        feeds: [],
-        posts: [],
-      };
 
-      const elements = {
-        form: document.querySelector('form'),
-        input: document.getElementById('url-input'),
-        submitButton: document.querySelector('button[type="submit"]'),
-        feedbackString: document.querySelector('.feedback'),
-        posts: document.querySelector('.posts'),
-        feeds: document.querySelector('.feeds'),
-        modalTitle: document.querySelector('.modal-title'),
-        modalDescription: document.querySelector('.modal-body'),
-        modalLink: document.querySelector('.full-article'),
-      };
+  await i18nInstance.init({
+    lng: 'ru',
+    debug: false,
+    resources,
+  });
 
-      const state = onChange(
-        initialState,
-        render(elements, initialState, i18nInstance),
-      );
+  const initialState = {
+    ui: {
+      submitDisabled: false,
+      readedPosts: new Set(),
+      modalWindow: null,
+    },
+    formStatus: 'filling',
+    error: '',
+    feeds: [],
+    posts: [],
+  };
 
-      yup.setLocale({
-        mixed: {
-          notOneOf: 'dublicatedURL',
-        },
-        string: {
-          url: 'invalidURL',
-        },
-      });
+  const elements = {
+    form: document.querySelector('form'),
+    input: document.getElementById('url-input'),
+    submitButton: document.querySelector('button[type="submit"]'),
+    feedbackString: document.querySelector('.feedback'),
+    posts: document.querySelector('.posts'),
+    feeds: document.querySelector('.feeds'),
+    modalTitle: document.querySelector('.modal-title'),
+    modalDescription: document.querySelector('.modal-body'),
+    modalLink: document.querySelector('.full-article'),
+  };
 
-      const makeSchema = (feedLinks) => yup.string().url().notOneOf(feedLinks);
+  const state = onChange(
+    initialState,
+    render(elements, initialState, i18nInstance),
+  );
 
-      elements.form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        state.ui.submitDisabled = true;
-        state.formStatus = 'sending';
-        const addedLinks = state.feeds.map((feed) => feed.link);
-        const schema = makeSchema(addedLinks);
-        const formData = new FormData(e.target);
-        const input = formData.get('url').trim();
-        schema
-          .validate(input)
-          .then(() => getAxiosResponse(input))
-          .then((response) => {
-            const { feed, posts } = parse(response.data.contents);
-            feed.link = input;
-            feed.id = uniqueId();
-            const postsWithIds = posts.map((post) => (
-              { ...post, id: uniqueId(), feedId: feed.id }
-            ));
-            state.feeds.push(feed);
-            state.posts.push(...postsWithIds);
-            state.formStatus = 'addedURL';
-            state.ui.submitDisabled = false;
-          })
-          .catch((err) => {
-            state.error = handleError(err);
-            state.formStatus = 'invalidURL';
-            console.log(state.error);
-          })
-          .finally(() => {
-            state.ui.submitDisabled = false;
-          });
-      });
-      elements.posts.addEventListener('click', (e) => {
-        const targetPostId = e.target.dataset.id;
-        state.ui.readedPosts.add(targetPostId);
-        state.ui.modalWindow = targetPostId;
-      });
-      updatePosts(state);
-    });
+  yup.setLocale({
+    mixed: {
+      notOneOf: 'dublicatedURL',
+    },
+    string: {
+      url: 'invalidURL',
+    },
+  });
+
+  const makeSchema = (feedLinks) => yup.string().url().notOneOf(feedLinks);
+
+  elements.form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    state.ui.submitDisabled = true;
+    state.formStatus = 'sending';
+    const addedLinks = state.feeds.map((feed) => feed.link);
+    const schema = makeSchema(addedLinks);
+    const formData = new FormData(e.target);
+    const input = formData.get('url').trim();
+    try {
+      await schema.validate(input);
+      const response = await getAxiosResponse(input);
+      const { feed, posts } = parse(response.data.contents);
+      feed.link = input;
+      feed.id = uniqueId();
+      const postsWithIds = posts.map((post) => (
+        { ...post, id: uniqueId(), feedId: feed.id }
+      ));
+      state.feeds.push(feed);
+      state.posts.push(...postsWithIds);
+      state.formStatus = 'addedURL';
+      state.ui.submitDisabled = false;
+    } catch (err) {
+      state.error = handleError(err);
+      state.formStatus = 'invalidURL';
+      console.log(state.error);
+    } finally {
+      state.ui.submitDisabled = false;
+    }
+  });
+  elements.posts.addEventListener('click', (e) => {
+    const targetPostId = e.target.dataset.id;
+    state.ui.readedPosts.add(targetPostId);
+    state.ui.modalWindow = targetPostId;
+  });
+  await updatePosts(state);
 };
